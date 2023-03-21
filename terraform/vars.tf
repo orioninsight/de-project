4d914aa30d3b45d87649bda734998fb269c2ee31
AWAIKEN1 <alistairaiken@gmail.com>
2023-03-21 11:09:17 +0000

    created variable for ingestion_zone_bucket

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,8 @@ variable "code_bucket_prefix" {
     type = string
     default = "lambda-code-"
 }
+
+variable "ingestion_zone_bucket_prefix" {
+    type = string
+    default = "ingestion-zone-"
+}
