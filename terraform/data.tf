2e9dddf95690975547f1d984ccb765223bcb7d57
Thomas Deakin <thomas.deakin@outlook.com>
2023-03-21 16:01:13 +0000

    work on terraform data dependencies

---
+++ terraform/data.tf
@@ -0,0 +1,10 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "archive_file" "ingestion_lambda_archive" {
+  type             = "zip"
+  source_file      = "${path.module}/../src/ingestion_lambda/${var.ingestion_lambda_name}.py"
+  output_path      = "${path.module}/../${var.ingestion_lambda_name}.zip"
+}
+
