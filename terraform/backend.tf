67bc02ec6e6fb85918f1fea0eb6e2cc735ac399c
AWAIKEN1 <alistairaiken@gmail.com>
2023-03-21 10:57:10 +0000

    adjusted S3 bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "nc-terraform-state-1679394142q"
+        bucket = "nc-terraform-state-1679394142"
         key = "s3_file_reader/terraform.tfstate"
         region = "us-east-1"
     }
