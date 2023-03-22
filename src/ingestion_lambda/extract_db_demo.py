61bc8afe90053dd26bb4366d0d6f1838b808773c
Thomas Deakin <thomas.deakin@outlook.com>
2023-03-22 17:21:52 +0000

    Revert "Demo files deleted"
    This reverts commit 48ece70f1ce135ceb50d7d24e8d6a8b2e9881440.

---
+++ src/ingestion_lambda/extract_db_demo.py
@@ -0,0 +1,10 @@
+import logging
+
+
+def extract_db_handler(event, context):
+    logger = logging.getLogger('MyLogger')
+    logger.setLevel(logging.INFO)
+
+    logger.info(f'testing: lambda has successfully ran')
+
+    print('We think this works')
