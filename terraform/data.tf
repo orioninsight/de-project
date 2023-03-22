5a3f82e68beaca9ba6c35e917d3e646bea4a4d17
Thomas Deakin <thomas.deakin@outlook.com>
2023-03-22 12:32:21 +0000

    formatting

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,8 +3,8 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "archive_file" "ingestion_lambda_archive" {
-  type             = "zip"
-  source_file      = "${path.module}/../src/ingestion_lambda/${var.ingestion_lambda_name}.py"
-  output_path      = "${path.module}/../${var.ingestion_lambda_name}.zip"
+  type        = "zip"
+  source_file = "${path.module}/../src/ingestion_lambda/${var.ingestion_lambda_name}.py"
+  output_path = "${path.module}/../${var.ingestion_lambda_name}.zip"
 }
 
