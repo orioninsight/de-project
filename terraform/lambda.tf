48dcac9819f60e8b72d57721534f29086b4c8385
AWAIKEN1 <alistairaiken@gmail.com>
2023-03-22 10:44:25 +0000

    Deploying ingestion lambda in terraform

---
+++ terraform/lambda.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "ingestion_lambda" {
+  function_name    = var.ingestion_lambda_name
+  role             = aws_iam_role.ingestion_lambda_role.arn
+  handler          = var.ingestion_lambda_handler
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256(data.archive_file.ingestion_lambda_archive.output_path)
+
+  environment {
+    variables = {
+      LOG_GROUP_NAME = resource.aws_cloudwatch_log_group.ingestion_lambda_log.name
+    }
+  }
+
+  // Here's where we specify the code location
+  s3_bucket = aws_s3_bucket.code_bucket.bucket
+  s3_key    = aws_s3_object.ingestion_lambda_code.key
+
+  depends_on = [
+    aws_s3_object.ingestion_lambda_code
+  ]
+}
+
+
