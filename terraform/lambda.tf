2b2cb79ba35c3cddd9ab093d5ea0d053bd5c7121
abanie <abanie@gmail.com>
2023-03-23 12:15:19 +0000

    update the lambda adding event trigger

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,3 +18,21 @@ resource "aws_lambda_function" "ingestion_lambda" {
     }
 }
 }
+
+resource "aws_cloudwatch_event_rule" "scheduler" {
+    name_prefix = "ingestion-scheduler-"
+    schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_lambda_permission" "allow_scheduler" {
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ingestion_lambda.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.scheduler.arn
+  source_account = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.scheduler.name
+  arn       = aws_lambda_function.ingestion_lambda.arn
+}
