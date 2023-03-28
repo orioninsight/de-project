65d0733ebe560421cda181935bfb89cedc46057b
AWAIKEN1 <alistairaiken@gmail.com>
2023-03-28 09:07:58 +0100

    updated bucket number for new sandbox

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nc-terraform-state-1679934113" #This will need to be updated per sandbox
+    bucket = "nc-terraform-state-1679990693" #This will need to be updated per sandbox
     key    = "tote-application/terraform.tfstate"
     region = "us-east-1"
   }
