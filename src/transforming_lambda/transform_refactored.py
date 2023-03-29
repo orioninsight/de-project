475347af64896a21adc5362b5801780d9a551e3b
esther615kim <vilchen@naver.com>
2023-03-29 17:10:20 +0100

    create transform_refactored.py to refactor transform.py

---
+++ src/transforming_lambda/transform_refactored.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import boto3
+import os
+import json
+import logging
+import datetime
+
+
+logger = logging.getLogger('MyLogger')
+logger.setLevel(logging.INFO)
+
+
+def transform_lambda_handler(event, context):
+    transformer = Transformer()
+    files = transformer.list_csv_files()
+    data_frames = transformer.read_csv(files)
+    # merge_data_frames(data_frames)
+
+
+class Transformer:
+    def __init__(self):
+        self.s3_client = boto3.client('s3')
+        self.s3_bucket_json = json.loads(os.environ['OI_STORER_SECRET_STRING'])
+        self.ingestion_bucket = self.s3_client.Bucket(
+            self.s3_bucket_json['s3_bucket_name'])
+
+    def list_csv_files(self):
+        s3 = boto3.resource('s3')
+        self.ingestion_bucket = s3.Bucket(
+            self.s3_bucket_json['s3_bucket_name'])
+
+        file_list = ['address', 'design', 'counterparty',
+                     'purchase_order', 'staff', 'sales_order',
+                     'payment', 'transaction', 'payment_type',
+                     'currency', 'department']
+
+        ingestion_csv_files = self.ingestion_bucket.objects.all()
+        # print(ingestion_csv_files)
+
+        for file in file_list:
+            # print(summary_object)
+            if file not in [item.key for item in ingestion_csv_files]:
+                raise Exception('files are not complete')
+
+        return [item.key for item in ingestion_csv_files]
+
+    def read_csv(self, key):
+        s3 = boto3.client('s3')
+        s3_bucket_json = json.loads(os.environ['OI_STORER_SECRET_STRING'])
+        ingestion_bucket = s3_bucket_json['s3_bucket_name']
+
+        try:
+
+            obj = s3.get_object(Bucket=ingestion_bucket, Key=key)
+            df = pd.read_csv(obj['Body'])
+            return df
+
+        except Exception as e:
+            logger.error(f'An error occurred reading csv file: {e}')
+            raise Exception()
+
+    def transform_currency(self, df_currency):
+        return df_currency.drop(columns=['created_at', 'last_updated'])
+
+    def transform_design(self, df_design):
+        return df_design.drop(columns=['created_at', 'last_updated'])
+
+    def transform_address(self, df_address):
+        return df_address.drop(columns=['created_at', 'last_updated']).rename(
+            columns={'address_id': 'location_id'})
+
+    def create_dim_date(self):
+        df = pd.DataFrame(pd.date_range(
+            '3/11/2022', '1/5/2023'), columns=['date'])
+        df['date_id'] = df['date'].dt.strftime('%Y%m%d').astype(int)
+        df['year'] = df['date'].dt.year
+        df['month'] = df['date'].dt.month
+        df['day'] = df['date'].dt.day
+        df['day_of_week'] = df['date'].dt.dayofweek
+        df['day_name'] = df['date'].dt.strftime("%A")
+        df['month_name'] = df['date'].dt.strftime("%B")
+        df['quarter'] = df['date'].dt.quarter
+
+        return df.loc[:, df.columns != 'date']
+
+    def merge_data_frames():
+        pass
+
+    def store_parquet():
+        pass
