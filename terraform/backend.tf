0064073c488f910d7211fbde25fb1c2f99a54c52
Thomas Deakin <thomas.deakin@outlook.com>
2023-03-31 10:46:30 +0100

    update backend bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nc-terraform-state-1680167954" #This will need to be updated per sandbox
+    bucket = "nc-terraform-state-1680251236" #This will need to be updated per sandbox
     key    = "tote-application/terraform.tfstate"
     region = "us-east-1"
   }
