a53c244e207ac0b7a24e5fe9eff4f05d9b209a1e
Thomas Deakin <thomas.deakin@outlook.com>
2023-04-02 13:59:05 +0100

    ensure names of resources follow consistent naming

--- terraform/IAM.tf
+++ terraform/IAM.tf
@@ -1,5 +1,10 @@
-#creates policy document locally for ingestion_lambda to access required S3 resources
-data "aws_iam_policy_document" "ingestion_lambda_bucket_access" {
+#############################
+### extraction lambda IAM ###
+#############################
+
+# create policy document locally for extraction_lambda to access required S3 resources
+data "aws_iam_policy_document" "extraction_lambda_bucket_access" {
+  # get code from code bucket
   statement {
 
     actions = ["s3:GetObject"]
@@ -8,43 +13,49 @@ data "aws_iam_policy_document" "ingestion_lambda_bucket_access" {
       "${aws_s3_bucket.code_bucket.arn}/extraction_lambda/*"
     ]
   }
+
+  # put, get objects, and list bucket contents in extraction zone bucket
   statement {
-
-    actions = ["s3:PutObject", "s3:GetObject"]
-
-    resources = [
-      "{aws_s3_bucket.ingestion_zone_bucket.arn}/*"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket"
     ]
-  }
-  statement {
-    actions = ["s3:ListBucket"]
     resources = [
-      aws_s3_bucket.ingestion_zone_bucket.arn
+      "${aws_s3_bucket.ingestion_zone_bucket.arn}",
+      "${aws_s3_bucket.ingestion_zone_bucket.arn}/*"
     ]
   }
 }
 
-#creates above policy in IAM
-resource "aws_iam_policy" "ingestion_lambda_bucket_access" {
-  name_prefix = "s3-access-policy-extraction-lambda-"
-  policy      = data.aws_iam_policy_document.ingestion_lambda_bucket_access.json
+# create S3 policy in IAM
+resource "aws_iam_policy" "extraction_lambda_bucket_access" {
+  name_prefix = "extraction-lambda-s3-access-policy"
+  policy      = data.aws_iam_policy_document.extraction_lambda_bucket_access.json
 }
 
 
-#creates policy locally to allow ingestion_lambda lambda to utilise log group
-data "aws_iam_policy_document" "ingestion_lambda_cw_document" {
+#create policy locally to allow extraction_lambda lambda to utilise log group
+data "aws_iam_policy_document" "extraction_lambda_cw_document" {
+  # allow create log stream and log events in the group
   statement {
 
     actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
 
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${resource.aws_cloudwatch_log_group.ingestion_lambda_log.name}:*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${resource.aws_cloudwatch_log_group.extraction_lambda_log.name}:*"
     ]
   }
 }
 
-#creates policy locally to allow ingestion_lambda lambda to access specific secret in secrets manager 
-data "aws_iam_policy_document" "ingestion_lambda_secretsmanager_document" {
+# creates cw policy in IAM
+resource "aws_iam_policy" "extraction_lamba_cw_policy" {
+  name_prefix = "extraction-lambda-cw-policy"
+  policy      = data.aws_iam_policy_document.extraction_lambda_cw_document.json
+}
+
+#creates policy locally to allow extraction_lambda lambda to access specific database secret from secrets manager 
+data "aws_iam_policy_document" "extraction_lambda_secretsmanager_document" {
   statement {
 
     actions = ["secretsmanager:GetSecretValue"]
@@ -55,20 +66,14 @@ data "aws_iam_policy_document" "ingestion_lambda_secretsmanager_document" {
   }
 }
 
-# creates above policy in IAM
-resource "aws_iam_policy" "ingestion_lamba_cw_policy" {
-  name_prefix = "cw-policy-extraction-lambda-"
-  policy      = data.aws_iam_policy_document.ingestion_lambda_cw_document.json
+# creates policy for extraction lambda to access db secret secretsmanager in IAM
+resource "aws_iam_policy" "extraction_lambda_secretsmanager_policy" {
+  name_prefix = "extraction-lambda-secretsmanager-policy"
+  policy      = data.aws_iam_policy_document.extraction_lambda_secretsmanager_document.json
 }
 
-# creates policy for lambda to access secretsmanager in IAM
-resource "aws_iam_policy" "ingestion_lamba_secretsmanager_policy" {
-  name_prefix = "secretsmanager-policy-extraction-lambda-"
-  policy      = data.aws_iam_policy_document.ingestion_lambda_secretsmanager_document.json
-}
-
-#creates policy locally to allow ingestion_lambda lambda to call transform_lambda
-data "aws_iam_policy_document" "ingestion_lambda_call_transform_document" {
+#creates policy locally to allow extraction_lambda lambda to invoke transform_lambda
+data "aws_iam_policy_document" "extraction_lambda_invoke_transform_document" {
   statement {
 
     actions = ["lambda:InvokeFunction"]
@@ -79,14 +84,14 @@ data "aws_iam_policy_document" "ingestion_lambda_call_transform_document" {
   }
 }
 
-# creates above policy in IAM
-resource "aws_iam_policy" "ingestion_lamba_call_transform_policy" {
-  name_prefix = "call-transform-policy-extraction-lambda-"
-  policy      = data.aws_iam_policy_document.ingestion_lambda_call_transform_document.json
+# creates invoke policy in IAM
+resource "aws_iam_policy" "extraction_lambda_invoke_transform_policy" {
+  name_prefix = "extraction-lambda-invoke-transform-policy"
+  policy      = data.aws_iam_policy_document.extraction_lambda_invoke_transform_document.json
 }
 
-# creates lambda role 
-resource "aws_iam_role" "ingestion_lambda_role" {
+# creates lambda role for extraction lambda
+resource "aws_iam_role" "extraction_lambda_role" {
   name_prefix        = "role-extraction-lambda-"
   assume_role_policy = <<EOF
     {
@@ -108,30 +113,37 @@ resource "aws_iam_role" "ingestion_lambda_role" {
     EOF
 }
 
-# attach IAM policy to lambda role 
-resource "aws_iam_role_policy_attachment" "ingestion_lambda_s3_policy_attachment" {
-  role       = aws_iam_role.ingestion_lambda_role.name
-  policy_arn = aws_iam_policy.ingestion_lambda_bucket_access.arn
+# attach extraction S3 policy to extration lambda role
+resource "aws_iam_role_policy_attachment" "extraction_lambda_s3_policy_attachment" {
+  role       = aws_iam_role.extraction_lambda_role.name
+  policy_arn = aws_iam_policy.extraction_lambda_bucket_access.arn
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_lambda_cw_policy_attachment" {
-  role       = aws_iam_role.ingestion_lambda_role.name
-  policy_arn = aws_iam_policy.ingestion_lamba_cw_policy.arn
+# attach extraction cw policy to extration lambda role
+resource "aws_iam_role_policy_attachment" "extraction_lambda_cw_policy_attachment" {
+  role       = aws_iam_role.extraction_lambda_role.name
+  policy_arn = aws_iam_policy.extraction_lamba_cw_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_lambda_secretsmanager_policy_attachment" {
-  role       = aws_iam_role.ingestion_lambda_role.name
-  policy_arn = aws_iam_policy.ingestion_lamba_secretsmanager_policy.arn
+# attach extraction secret manager policy to extration lambda role
+resource "aws_iam_role_policy_attachment" "extraction_lambda_secretsmanager_policy_attachment" {
+  role       = aws_iam_role.extraction_lambda_role.name
+  policy_arn = aws_iam_policy.extraction_lambda_secretsmanager_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_lambda_call_transform_policy_attachment" {
-  role       = aws_iam_role.ingestion_lambda_role.name
-  policy_arn = aws_iam_policy.ingestion_lamba_call_transform_policy.arn
+# attach extraction invoke policy to extration lambda role
+resource "aws_iam_role_policy_attachment" "extraction_lambda_invoke_transform_policy_attachment" {
+  role       = aws_iam_role.extraction_lambda_role.name
+  policy_arn = aws_iam_policy.extraction_lambda_invoke_transform_policy.arn
 }
+
+############################
 ### trasformation_lambda ###
+############################
 
-#creates policy document locally for transform_lambda to access required S3 resources
+# creates policy document locally for transform_lambda to access required S3 resources
 data "aws_iam_policy_document" "transform_lambda_bucket_access" {
+  # get objects from code bucket
   statement {
 
     actions = ["s3:GetObject"]
@@ -141,7 +153,7 @@ data "aws_iam_policy_document" "transform_lambda_bucket_access" {
     ]
   }
   statement {
-
+    # get and list objects from extraction zone bucket
     actions = ["s3:GetObject", "s3:ListBucket"]
 
     resources = [
@@ -160,7 +172,7 @@ data "aws_iam_policy_document" "transform_lambda_bucket_access" {
 
 #creates above policy in IAM
 resource "aws_iam_policy" "transform_lambda_bucket_access" {
-  name_prefix = "s3-access-policy-transform-lambda-"
+  name_prefix = "transform-lambda-s3-access-policy"
   policy      = data.aws_iam_policy_document.transform_lambda_bucket_access.json
 }
 
@@ -176,9 +188,9 @@ data "aws_iam_policy_document" "transform_lambda_cw_document" {
   }
 }
 
-# creates above policy in IAM
+# creates cw policy in IAM
 resource "aws_iam_policy" "transform_lamba_cw_policy" {
-  name_prefix = "cw-policy-transform-lambda-"
+  name_prefix = "transform-lambda-cw-policy"
   policy      = data.aws_iam_policy_document.transform_lambda_cw_document.json
 }
 
@@ -207,12 +219,13 @@ resource "aws_iam_role" "transform_lambda_role" {
     EOF
 }
 
-# attach IAM policy to transform-lambda role 
+# attach s3 policy to transform-lambda role 
 resource "aws_iam_role_policy_attachment" "transform_lambda_s3_policy_attachment" {
   role       = aws_iam_role.transform_lambda_role.name
   policy_arn = aws_iam_policy.transform_lambda_bucket_access.arn
 }
 
+# attach cw policy to transform-lambda role
 resource "aws_iam_role_policy_attachment" "transform_lambda_cw_policy_attachment" {
   role       = aws_iam_role.transform_lambda_role.name
   policy_arn = aws_iam_policy.transform_lamba_cw_policy.arn
--- terraform/logs.tf
+++ terraform/logs.tf
@@ -1,4 +1,5 @@
-resource "aws_cloudwatch_log_group" "ingestion_lambda_log" {
+# creates a log group for extraction lambda
+resource "aws_cloudwatch_log_group" "extraction_lambda_log" {
   name = "/aws/lambda/extraction_lambda"
 
   tags = {
@@ -6,6 +7,7 @@ resource "aws_cloudwatch_log_group" "ingestion_lambda_log" {
   }
 }
 
+# creates a log group for transform lambda
 resource "aws_cloudwatch_log_group" "transform_lambda_log" {
   name = "/aws/lambda/transform_lambda"
 
